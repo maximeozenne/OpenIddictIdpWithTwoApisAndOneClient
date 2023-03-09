42c1de76d0d227c6616522cf2e35df49ab9dfcf2
Maxime Ozenne <maxoz608@gmail.com>
2023-03-09 17:42:05 +0100

    Configure the api to be callable through the client credentials flow

--- StandaloneApi/Controllers/StandaloneDataController.cs
+++ StandaloneApi/Controllers/StandaloneDataController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OpenIddict.Validation.AspNetCore;
 
 namespace StandaloneApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[Authorize(AuthenticationSchemes = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme)]
 public class StandaloneDataController : ControllerBase
 {
     public StandaloneDataController() {}
--- StandaloneApi/Program.cs
+++ StandaloneApi/Program.cs
@@ -1,4 +1,35 @@
+using OpenIddict.Validation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+});
+
+builder.Services
+    .AddOpenIddict()
+    .AddValidation(options =>
+    {
+        // Note: the validation handler uses OpenID Connect discovery
+        // to retrieve the address of the introspection endpoint.
+        options.SetIssuer("https://localhost:5443/");
+        options.AddAudiences("standaloneapi");
+
+        // Configure the validation handler to use introspection and register the client
+        // credentials used when communicating with the remote introspection endpoint.
+        options.UseIntrospection()
+                .SetClientId("standaloneapi")
+                .SetClientSecret("standaloneapi-secret");
+
+        // disable access token encyption for this
+        options.UseAspNetCore();
+
+        // Register the System.Net.Http integration.
+        options.UseSystemNetHttp();
+    });
+
+builder.Services.AddAuthorization();
 
 // Add services to the container.
 
@@ -10,6 +41,9 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
